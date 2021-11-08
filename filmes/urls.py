8f370dee43192ed972559ec72c536dcd606a1b37
Felipe <felipebpl@al.insper.edu.br>
2021-11-08 10:23:34 -0300

    url

--- filmes/urls.py
+++ filmes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:filme_id>/', views.api_filme),
+    path('minhalista/<int:filme_id>/', views.api_filme),
     # path('', views.api_filme)
 ]
